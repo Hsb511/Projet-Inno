040cdfbd790818c4109f721423492b209f0f93e7
Guillaume Weghsteen <guillaume.weghsteen@telecom-bretagne.eu>
2017-11-21 10:51:30 +0100

    ajout du fichier de connexion

---
+++ connexion.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8" />
+
+        <title>Proxygeia - connexion à votre espace perso</title>
+
+    </head>
+
+
+    <body>
+    fichier proposant la connexion à son espace perso
+
+
+        
+
+    </body>
+
+</html>
