2c6176700509db395075a1f88de7b5001c8625a6
Yuqing <yuqing.hu@telecom-bretagne.eu>
2017-11-24 09:32:40 +0100

    changer userMenu.php

--- user/userMenu.php
+++ user/userMenu.php
@@ -1,7 +1,12 @@
 <nav class="align-self-m conteneur-v">
-<?php		
-	echo "<p class='marge-v-l'><a href='main.php?section=pad&page=askForHelp' class='text-menu'>Demander une aide pour moi</a></p>";
-	echo "<p class='marge-v-l'><a href='main.php?section=helper&page=listAnnounces' class='text-menu'>Proposer de l'aide</a></p>";
-	echo "<p class='marge-v-l'><a href='main.php?section=familly&page=manageRelatives' class='text-menu'>Gérer ma famille</a></p>";
+<?php
+    echo "<table border='0' cellspacing='0px' width='1000px'>";
+	echo "<tr><th><span class='marge-v-l'><a href='main.php?section=pad&page=askForHelp' class='text-menu'>Demander une aide pour moi</a></span></th>
+	<th><span class='marge-v-l'><a href='main.php?section=helper&page=listAnnounces' class='text-menu'>Proposer de l'aide</a></span></th>
+	<th><span class='marge-v-l'><a href='main.php?section=familly&page=manageRelatives' class='text-menu'>Gérer ma famille</a></span></th></tr>";
+	echo "</table>";	
+	//echo "<p class='marge-v-l'><a href='main.php?section=pad&page=askForHelp' class='text-menu'>Demander une aide pour moi</a></p>";
+	//echo "<p class='marge-v-l'><a href='main.php?section=helper&page=listAnnounces' class='text-menu'>Proposer de l'aide</a></p>";
+	//echo "<p class='marge-v-l'><a href='main.php?section=familly&page=manageRelatives' class='text-menu'>Gérer ma famille</a></p>";
 ?>
 </nav>
