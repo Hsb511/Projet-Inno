61b98d3dd5a984e65662d0f301b672a21f49cdcd
Laure Souche <laure.souche@telecom-bretagne.eu>
2017-11-25 19:26:06 +0100

    finitions sur la page de connexion

--- account/connection.php
+++ account/connection.php
@@ -24,41 +24,64 @@ if (isset($_GET["restartDemo"]) && $_GET["restartDemo"]){
             <!-- Contact social -->                                      
     <div style="text-align: center;">
         <form action="user/main.php" method="post" style="width : 265px; margin : auto;">
-        <div style="width : 265px; padding : 5px; max-width : calc(100% - 12px); margin: 4px 0px 4px 0px; line-height: 21px; font-size :20px; text-align : center;"> Nom d'utilisateur : </div>
-        <input type="text" name="username" style="border-color: 
-        black; background-color :rgba(0, 0, 0, 0.1); border-width :
-         1px; border-style :solid; border-radius : 3px; width : 265px; 
-         padding : 5px; box-shadow : rgb(69, 85, 108) 1px 1px 0px 0px; max-width : calc(100% - 12px); margin: 4px 0px 4px 0px; line-height: 21px; font-size :16px;"> <br/    >
-        <div style="width : 265px; padding : 5px; max-width : calc(100% - 12px); margin: 4px 0px 4px 0px; line-height: 21px; font-size :20px; text-align : center;"> Mot de passe : </div>
-        <input type="password" name="password" style="border-color: 
-        black; background-color :rgba(0, 0, 0, 0.1); border-width :
-         1px; border-style :solid; border-radius : 3px; width : 265px; 
-         padding : 5px; box-shadow : rgb(69, 85, 108) 1px 1px 0px 0px; max-width : calc(100% - 12px); margin: 4px 0px 4px 0px; line-height: 21px; font-size :16px;"> <br/>
-        
-        <input type="submit" value="Entrer dans mon espace" style="border-color: 
-        black; background-color :rgba(0, 0, 0, 0.1); border-width :
-         1px; border-style :solid; border-radius : 3px; width : auto; 
-         padding : 5px; box-shadow : rgb(69, 85, 108) 1px 1px 0px 0px; margin: 50px 0px 4px 0px; line-height: 21px; font-size :16px;">
+            <div style="width : 265px; padding : 5px; max-width : calc(100% - 12px); margin: 4px 0px 4px 0px; line-height: 21px; font-size :20px; text-align : center;"> Nom d'utilisateur : </div>
+            <input type="text" name="username" style="border-color: 
+            black; background-color :rgba(0, 0, 0, 0.1); border-width :
+                1px; border-style :solid; border-radius : 3px; width : 265px; 
+                padding : 5px; box-shadow : rgb(69, 85, 108) 1px 1px 0px 0px; max-width : calc(100% - 12px); margin: 4px 0px 4px 0px; line-height: 21px; font-size :16px;"> <br/    >
+            <div style="width : 265px; padding : 5px; max-width : calc(100% - 12px); margin: 4px 0px 4px 0px; line-height: 21px; font-size :20px; text-align : center;"> Mot de passe : </div>
+            <input type="password" name="password" style="border-color: 
+            black; background-color :rgba(0, 0, 0, 0.1); border-width :
+                1px; border-style :solid; border-radius : 3px; width : 265px; 
+                padding : 5px; box-shadow : rgb(69, 85, 108) 1px 1px 0px 0px; max-width : calc(100% - 12px); margin: 4px 0px 4px 0px; line-height: 21px; font-size :16px;"> <br/>
+            
+            <input type="submit" value="Entrer dans mon espace" style="border-color: 
+            black; background-color :rgba(0, 0, 0, 0.1); border-width :
+                1px; border-style :solid; border-radius : 3px; width : auto; 
+                padding : 5px; box-shadow : rgb(69, 85, 108) 1px 1px 0px 0px; margin: 50px 0px 4px 0px; line-height: 21px; font-size :16px;">
 
         </form>
     </div>
+</li>
+<li>
+    <div >
+        <div class="s-title-group  " style="padding-top :80px; padding-bottom : 80px;">
+            <div class="s-title ">
+                <div class="s-component s-text">
+                    <h2 class="">
+                        <div class="s-component-content needsclick recursive s-font-title"
+                                style="outline: 0px;"   tabindex="0"   role="textbox"
+                                aria-label="false">
+                                <p> Comptes de démo </p>
+                        </div>
+                    </h2>
+                </div>
+            </div>        
+        </div>
+    </div>
+    <div style="margin : auto; width: 400px; text-align: center; font-size: 20px; color: rgb(51 ,51, 51); font-weight: 400;">
+        <p style= "line-height: 29px;">Paulette "PauletteDubois" Dubois <br/></p>
+        <p style= "line-height: 29px;">Véronique "Vero45" Duval <br/></p>
+        <p style= "line-height: 29px;">Maxime "max38" Turtille <br/></p>
+        <!-- pour réinitialiser la démonstration-->
+        <form action="index.php" method="get">
+            <input type="hidden" name="restartDemo" value="true">
+            <input type="hidden" name="page" value="connection">
+            <input type="submit" value="redémarrer la démo" style="border-color: 
+                black; background-color :rgba(0, 0, 0, 0.1); border-width :
+                    1px; border-style :solid; border-radius : 3px; width : auto; 
+                    padding : 5px; box-shadow : rgb(69, 85, 108) 1px 1px 0px 0px; margin: 50px 0px 4px 0px; line-height: 21px; font-size :16px;">
+            
+        </form>
+    </div>
+
+
                     
                 
     
+    
 
-    <p><u>Comptes de démo :</u> <br/>
-        Paulette "PauletteDubois" Dubois <br/>
-        Véronique "Vero45" Duval <br/>
-        Maxime "max38" Turtille <br/></p>
 
-    <!-- pour réinitialiser la démonstration-->
-    <form action="index.php" method="get">
-        <input type="hidden" name="restartDemo" value="true">
-        <input type="hidden" name="page" value="connection">
-        <input type="submit" value="redémarrer la démo">
-        
-    </form>
-</html>
 
 <?php
     /*structure du $_SESSION pour simuler la DB :
