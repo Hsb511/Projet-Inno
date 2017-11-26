98767d112f9bfcf86c5feb30a71a2afd477c7568
Laure Souche <laure.souche@telecom-bretagne.eu>
2017-11-26 13:07:17 +0100

    homogenisation de la barre de navigation

--- account/index.php
+++ account/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <!-- saved from url=(0048)https://www.strikingly.com/s/sites/11615441/edit -->
-<html itemscope="" itemtype="http://schema.org/WebPage" lang="fr" xmlns:fb="https://www.facebook.com/2008/fbml" xmlns:og="http://ogp.me/ns#" xmlns="http://www.w3.org/1999/xhtml" class="wf-lato-n4-active wf-lato-n7-active wf-lato-i4-active wf-opensans-n4-active wf-opensans-n7-active wf-lora-i4-active wf-lora-i7-active wf-lora-n4-active wf-lora-n7-active wf-montserrat-n4-active wf-montserrat-n7-active wf-nunito-n4-active wf-nunito-n7-active wf-playfairdisplay-n4-active wf-playfairdisplay-n7-active wf-playfairdisplay-i4-active wf-playfairdisplay-i7-active wf-roboto-n3-active wf-roboto-n7-active wf-roboto-i7-active wf-lato-n3-active wf-dancingscript-n4-active wf-arvo-n7-active wf-abrilfatface-n4-active wf-lato-i7-active wf-worksans-n7-active wf-worksans-n6-active wf-worksans-n4-active wf-arvo-n4-active wf-vollkorn-i4-active wf-vollkorn-n4-active wf-unna-n4-active wf-titilliumweb-i7-active wf-titilliumweb-i3-active wf-titilliumweb-n7-active wf-titilliumweb-n3-active wf-sueellenfrancisco-n4-active wf-sourcesanspro-i7-active wf-sourcesanspro-i6-active wf-sourcesanspro-i4-active wf-sourcesanspro-i3-active wf-sourcesanspro-n7-active wf-sourcesanspro-n6-active wf-sourcesanspro-n4-active wf-sourcesanspro-n3-active wf-robotoslab-n7-active wf-robotoslab-n3-active wf-robotocondensed-n7-active wf-robotocondensed-n3-active wf-roboto-i3-active wf-raleway-n7-active wf-raleway-n3-active wf-ptsansnarrow-n7-active wf-ptsansnarrow-n4-active wf-parisienne-n4-active wf-opensans-n6-active wf-opensans-n3-active wf-opensans-i7-active wf-opensans-i6-active wf-opensans-i4-active wf-opensans-i3-active wf-oldstandardtt-i4-active wf-oldstandardtt-n7-active wf-oldstandardtt-n4-active wf-josefinslab-i7-active wf-josefinslab-i4-active wf-josefinslab-n7-active wf-josefinslab-n4-active wf-istokweb-n7-active wf-istokweb-n4-active wf-istokweb-i7-active wf-istokweb-i4-active wf-greatvibes-n4-active wf-dancingscript-n7-active wf-chewy-n4-active wf-cardo-i4-active wf-cardo-n7-active wf-cardo-n4-active wf-bitter-i4-active wf-bitter-n7-active wf-bitter-n4-active wf-badscript-n4-active wf-arvo-i7-active wf-arvo-i4-active wf-archivonarrow-i7-active wf-archivonarrow-i4-active wf-archivonarrow-n7-active wf-archivonarrow-n4-active wf-lato-i3-active wf-active">
+<html itemscope="" itemtype="http://schema.org/WebPage" lang="fr" xmlns="http://www.w3.org/1999/xhtml" class="wf-lato-n4-active wf-lato-n7-active wf-lato-i4-active wf-opensans-n4-active wf-opensans-n7-active wf-lora-i4-active wf-lora-i7-active wf-lora-n4-active wf-lora-n7-active wf-montserrat-n4-active wf-montserrat-n7-active wf-nunito-n4-active wf-nunito-n7-active wf-playfairdisplay-n4-active wf-playfairdisplay-n7-active wf-playfairdisplay-i4-active wf-playfairdisplay-i7-active wf-roboto-n3-active wf-roboto-n7-active wf-roboto-i7-active wf-lato-n3-active wf-dancingscript-n4-active wf-arvo-n7-active wf-abrilfatface-n4-active wf-lato-i7-active wf-worksans-n7-active wf-worksans-n6-active wf-worksans-n4-active wf-arvo-n4-active wf-vollkorn-i4-active wf-vollkorn-n4-active wf-unna-n4-active wf-titilliumweb-i7-active wf-titilliumweb-i3-active wf-titilliumweb-n7-active wf-titilliumweb-n3-active wf-sueellenfrancisco-n4-active wf-sourcesanspro-i7-active wf-sourcesanspro-i6-active wf-sourcesanspro-i4-active wf-sourcesanspro-i3-active wf-sourcesanspro-n7-active wf-sourcesanspro-n6-active wf-sourcesanspro-n4-active wf-sourcesanspro-n3-active wf-robotoslab-n7-active wf-robotoslab-n3-active wf-robotocondensed-n7-active wf-robotocondensed-n3-active wf-roboto-i3-active wf-raleway-n7-active wf-raleway-n3-active wf-ptsansnarrow-n7-active wf-ptsansnarrow-n4-active wf-parisienne-n4-active wf-opensans-n6-active wf-opensans-n3-active wf-opensans-i7-active wf-opensans-i6-active wf-opensans-i4-active wf-opensans-i3-active wf-oldstandardtt-i4-active wf-oldstandardtt-n7-active wf-oldstandardtt-n4-active wf-josefinslab-i7-active wf-josefinslab-i4-active wf-josefinslab-n7-active wf-josefinslab-n4-active wf-istokweb-n7-active wf-istokweb-n4-active wf-istokweb-i7-active wf-istokweb-i4-active wf-greatvibes-n4-active wf-dancingscript-n7-active wf-chewy-n4-active wf-cardo-i4-active wf-cardo-n7-active wf-cardo-n4-active wf-bitter-i4-active wf-bitter-n7-active wf-bitter-n4-active wf-badscript-n4-active wf-arvo-i7-active wf-arvo-i4-active wf-archivonarrow-i7-active wf-archivonarrow-i4-active wf-archivonarrow-n7-active wf-archivonarrow-n4-active wf-lato-i3-active wf-active">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Proxygeia</title>
+    <title>Compte Proxygeia</title>
 
     <meta content="width=device-width,initial-scale=1.0,user-scalable=yes,minimum-scale=1.0,maximum-scale=3.0" id="viewport" name="viewport">
-    <link href="https://static-assets.strikinglycdn.com/images/icons/favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <link href="../Proxygeia_files/acax3ilq2tovi0m8k2gw.png" rel="shortcut icon" type="image/x-icon">
 
     <link rel="stylesheet" media="screen" href="../Proxygeia_files/editor.8d35e9c6b8cf8126cee9.bundle.css">
     <link rel="stylesheet" media="screen" href="../Proxygeia_files/main_v4_editor.6cf9cd30433bcfb50ea4.bundle.css">
@@ -15,14 +15,14 @@
     <meta name="csrf-token" content="bwTgmMX7SuGqhLDjdkTz0v5rd7JSPs+eFQLtVOpCCldmuMF/cJW4BKAEYbTremNnkMAxZXKOJej+hADXc5PS7Q==">
     <meta name="asset-url" content="https://static-assets.strikinglycdn.com">
     </head>
-<body class="locale-fr  s-mouse-enabled" id="sites">  
+<body class="edit locale-fr product-strikingly s-mouse-enabled" id="sites"> 
 
 <div id="s-content" class="s-variation-default  s-font-heading-default s-font-title-default s-font-body-default " lang="fr">
     <div class="s-navbar-section">
         <div class="navigator" id="header-container">
-            <div class="container">
-                <div class="columns sixteen">
-                    <div class="header-wrapper">
+            <div class="container" style="width:100%;">
+                <div class="columns sixteen" style="width:100%;">
+                    <div class="header-wrapper" style="width:100%;">
                         <div class="logo-container">
                         <div class="logo-image">
                             <div class="s-component s-image">
@@ -43,9 +43,9 @@
                             </div>
                         </div>
                     </div>
-                    <div class="nav-container">
-                        <ul class="s-nav">
-                            <span>
+                    <div class="nav-container" style="text-align : left;">
+                        <ul class="s-nav" style="font-size:23px; text-align :left;">
+                            <span style="text-align : left;">
                                 <li>
                                     <a class="s-nav-item" href="../Proxygeia.html#proxygeia">
                                         <span class="s-font-body">Proxygéia</span>
