7247ab379ff7daa9786a2681447c98a158cd1264
Hugo Selle <hugo.selle@telecom-bretagne.eu>
2017-11-28 10:08:26 +0100

    fix du nav bar

--- user/helper/listAnnounces.php
+++ user/helper/listAnnounces.php
@@ -1,4 +1,4 @@
-#liste les annonces d'aide postée sur le site par l'utilisateur. propose d'en rajouter ou d'en modifier.
+liste les annonces d'aide postée sur le site par l'utilisateur. propose d'en rajouter ou d'en modifier.
 
 <p> Liste de vos annonces </p>
 <a class='text-menu'>-I need my tomato!</a><br/>
