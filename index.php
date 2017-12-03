fc54343dddff9c22156db9d6eea9c994f9ce9d83
Hugo Selle <hugo.selle@telecom-bretagne.eu>
2017-12-03 21:55:52 +0100

    merge

--- index.php
+++ index.php
@@ -45,11 +45,7 @@
     $pageToInclude = "";
     // contenus des pages relatives au compte
     //page de connexion
-<<<<<<< HEAD
     if(isset($_GET["page"]) && ($_GET["page"]=="connection" || $_GET["page"]=="form")) {
-=======
-    if(isset($_GET["page"]) && ($_GET["page"] == "connection" || $_GET["page"] == "form")) {
->>>>>>> 5aca0d14e73b7af38fa65a43f88cc6e584b2d61a
         $menu = [["Proxygéia","#proxygeia","./Proxygeia.html"],["Fonctionnalités","#fonctionnalites","./Proxygeia.html"],["Témoignages","#temoignages","./Proxygeia.html"],["Particuliers","#particulier","./Proxygeia.html"],["Contactez-nous","#contact","./Proxygeia.html"]] ; 
         if ($_GET["page"]=="connection") {
            $pageToInclude = "connection.php" ;
