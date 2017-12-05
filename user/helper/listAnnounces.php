f368242378b8f24578a65f2f8971d85850ac0d56
Laure Souche <laure.souche@telecom-bretagne.eu>
2017-12-05 23:35:26 +0100

    mise en page de la liste des annonces

--- user/helper/listAnnounces.php
+++ user/helper/listAnnounces.php
@@ -19,40 +19,50 @@
 //Chaque annonce est formée de tâches régulières ou non à effectuer
 //parcourt la liste des annonces
 foreach($profile["announces"] as $key => $id){
-    $announce = $_SESSION["announces"][$id];
-
-    echo("<b>nom :".$announce['title']."</b>");                         
-
-    echo "description : ".$announce["description"]."<br/>";
-    echo "fréquence : ".$announce["frequency"]." fois par semaine (";
-    foreach($announce["days"] as $id => $day){
-        echo $day." ";
-    }
-    echo ") pendant ".$announce["duration"]." heures";
-    echo "<table>
-            <tr>
-                <th> type de taĉhe  </th>
-                <th>commentaire  </th>
-                <th>régulier</th>
-            </tr>";
-    print_r(count($announce["tasks"]));
-    //parcourt la liste des tâches de l'annonce
-    foreach($announce["tasks"] as $id => $taskID){    
-        echo ("
-        <tr>
-            <td>".$_SESSION["task_types"][ $_SESSION["tasks"][$taskID]["task_type"] ]."</td>
-            <td>".$_SESSION["tasks"][$taskID]["comment"]."</td>");
-            if($_SESSION["tasks"][$taskID]["repeat"] == 1){
-                echo"<td>à chaque visite</td>";
+    echo "</li><li>";
+        echo "<div style='margin : auto; width: 1000px; text-align: left; font-size: 20px; color: rgb(51 ,51, 51); font-weight: 400;'>";
+            $announce = $_SESSION["announces"][$id];
+            echo "<div style='padding : 5px; width :100%; margin: 4px 0px 4px 0px; line-height: 21px; font-size :20px; text-align : center;'> <b>Nom : ".$announce['title']."</b> </div>";
+            echo "<div style='padding: 20px 0px 20px 0px;'>";
+            echo "<div style='padding : 5px; width :100%; margin: 4px 0px 4px 0px; line-height: 21px; font-size :20px;'> <b>Description :</b> ".$announce["description"]."</div>";
+            $phase = "";
+            foreach($announce["days"] as $id => $day){   
+                if (count($announce["days"])-1 == $id){
+                    $phase= $phase.$day;    
+                }             
+                else {
+                    $phase= $phase.$day." "; 
+                }           
             }
-            elseif($_SESSION["tasks"][$taskID]["repeat"] == 2){
-                echo"<td>toutes les 2 visites</td>";
+            echo "<div 
+            style='padding : 5px; width :100%; margin: 4px 0px 4px 0px; line-height: 21px; font-size :20px;'> 
+            <b>Fréquence :</b>  ".$announce["frequency"]." fois par semaine (".$phase.") pendant ".$announce["duration"]." heures</div>";
+            echo "</div>";
+            
+            echo "<div style='padding : 5px; width :100%; margin: 4px 0px 4px 0px; line-height: 21px; font-size :20px;''><table style='width : 100%;' >
+                    <tr style='color :rgb(255, 111, 34); padding: 15px;'>
+                        <th align='center' style ='padding: 15px;'><b>Type de taĉhe </b> </th>
+                        <th align='center' style ='padding: 15px;'><b>Commentaire</b></th>
+                        <th align='center' style ='padding: 15px;'><b>Régulier</b></th>
+                    </tr>";
+            //parcourt la liste des tâches de l'annonce
+            foreach($announce["tasks"] as $id => $taskID){    
+                echo ("
+                <tr style ='padding: 15px;'>
+                    <td align='center' style ='padding: 15px;'>".$_SESSION["task_types"][ $_SESSION["tasks"][$taskID]["task_type"] ]."</td>
+                    <td align='center' style ='padding: 15px;'>".$_SESSION["tasks"][$taskID]["comment"]."</td>");
+                    if($_SESSION["tasks"][$taskID]["repeat"] == 1){
+                        echo"<td align='center' style ='padding: 15px;' >A chaque visite</td>";
+                    }
+                    elseif($_SESSION["tasks"][$taskID]["repeat"] == 2){
+                        echo"<td align='center' style ='padding: 15px;'>Toutes les 2 visites</td>";
+                    }
+                    else{
+                        echo"<td align='center' style ='padding: 15px;'>Seulement la prochaine visite</td>";
+                    }
+                    echo"</tr>";
             }
-            else{
-                echo"<td>seulement la prochaine visite</td>";
-            }
-            echo"</tr>";
-}
-echo "</table><br/><br/>";
+            echo "</table></div><br/><br/>";
+        echo "</div>";
 }
 ?>
